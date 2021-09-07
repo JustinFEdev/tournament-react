2b0a5f7537a7ad8f45ed07914cb2a94ea00d11a8
Justin_supertree <justin1688@supertree.co>
2021-09-07 15:36:52 +0900

    09/07 update

--- src/components/modal/GamePopup.js
+++ src/components/modal/GamePopup.js
@@ -17,18 +17,22 @@ const GamePopup = () => {
   `;
 
   useEffect(() => {
-    document.body.style.cssText = `
+    if (switchPop === true) {
+      document.body.style.cssText = `
       position: fixed;
       top: -${window.scrollY}px;
       overflow-y: scroll;
       width: 100%;`;
-    return () => {
-      const scrollY = document.body.style.top;
-      document.body.style.cssText = "";
-      window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
-    };
-  }, []);
-
+      return () => {
+        const scrollY = document.body.style.top;
+        document.body.style.cssText = "";
+        window.scrollTo(
+          0,
+          parseInt(scrollY || "0", 10) * -1
+        );
+      };
+    }
+  }, [switchPop]);
   return (
     <>
       {switchPop === true && (
