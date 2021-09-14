38da9952d7c714c97364cd76f275846f6085df06
Justin_supertree <justin1688@supertree.co>
2021-09-14 11:44:57 +0900

    09/14 update

--- src/components/slides/LiveSlide.js
+++ src/components/slides/LiveSlide.js
@@ -4,6 +4,12 @@ import "../navigation/navi.scss";
 import "./slide.scss";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile,
+} from "react-device-detect";
 
 const LiveSlide = ({ live }) => {
   const liveSettings = {
@@ -22,70 +28,162 @@ const LiveSlide = ({ live }) => {
     touchMove: true,
     useCSS: true,
   };
+  const mobileliveSettings = {
+    arrows: false,
+    dots: false,
+    infinite: true,
+    speed: 1500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    autoplay: false,
+    autoplaySpeed: 3500,
+    centerMode: false,
+    swipe: true,
+    swipeToSlide: false,
+    touchMove: true,
+    useCSS: true,
+  };
 
   return (
     <>
       {live !== undefined && (
         <>
-          <Slider {...liveSettings}>
-            {live.map((info, index) => (
-              <div className="live-container" key={index}>
-                <div className="live-wrapper">
-                  {/* <div className="live-livestatus">
+          {isBrowser ? (
+            <>
+              <Slider {...liveSettings}>
+                {live.map((info, index) => (
+                  <div
+                    className="live-container"
+                    key={index}
+                  >
+                    <div className="live-wrapper">
+                      {/* <div className="live-livestatus">
                     {info.live}
                   </div> */}
-                  <div className="live-timestatus">
-                    {info.time}
-                  </div>
-                </div>
-                <div className="live-imgarea">
-                  <img src={info.img} alt={info.img} />
-                </div>
-                <div className="live-info-wrapper">
-                  <div className="live-info-titlearea">
-                    <span className="live-info-title">
-                      {info.title}
-                    </span>
+                      <div className="live-timestatus">
+                        {info.time}
+                      </div>
+                    </div>
+                    <div className="live-imgarea">
+                      <img src={info.img} alt={info.img} />
+                    </div>
+                    <div className="live-info-wrapper">
+                      <div className="live-info-titlearea">
+                        <span className="live-info-title">
+                          {info.title}
+                        </span>
 
-                    <div className="live-wininfo">
-                      <span className="live-win">WIN</span>
-                      <span className="pp-wrapper">
-                        <p className="pp-text">PP</p>
-                      </span>
-                      <span className="live-point">
-                        {info.point}
-                      </span>
+                        <div className="live-wininfo">
+                          <span className="live-win">
+                            WIN
+                          </span>
+                          <span className="pp-wrapper">
+                            <p className="pp-text">PP</p>
+                          </span>
+                          <span className="live-point">
+                            {info.point}
+                          </span>
+                        </div>
+                      </div>
+                      <div className="live-liveinfo">
+                        <div
+                          className="live-"
+                          style={{
+                            borderTop: "1px solid",
+                            padding: "20px 0",
+                            display: "flex",
+                            justifyContent: "space-between",
+                          }}
+                        >
+                          <span
+                            style={{
+                              border: "1px solid",
+                              padding: "7px",
+                              borderRadius: 10,
+                              backgroundColor: "red",
+                              color: "#fff",
+                              fontWeight: "bold",
+                            }}
+                          >
+                            Live
+                          </span>
+                          <span>People</span>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                  <div className="live-liveinfo">
-                    <div
-                      className="live-"
-                      style={{
-                        borderTop: "1px solid",
-                        padding: "20px 0",
-                        display: "flex",
-                        justifyContent: "space-between",
-                      }}
-                    >
-                      <span
-                        style={{
-                          border: "1px solid",
-                          padding: "7px",
-                          borderRadius: 10,
-                          backgroundColor: "red",
-                          color: "#fff",
-                          fontWeight: "bold",
-                        }}
-                      >
-                        Live
-                      </span>
-                      <span>People</span>
+                ))}
+              </Slider>
+            </>
+          ) : (
+            <>
+              <Slider {...mobileliveSettings}>
+                {live.map((info, index) => (
+                  <div
+                    className="live-container"
+                    key={index}
+                  >
+                    <div className="live-wrapper">
+                      {/* <div className="live-livestatus">
+                {info.live}
+              </div> */}
+                      <div className="live-timestatus">
+                        {info.time}
+                      </div>
+                    </div>
+                    <div className="live-imgarea">
+                      <img src={info.img} alt={info.img} />
+                    </div>
+                    <div className="live-info-wrapper">
+                      <div className="live-info-titlearea">
+                        <span className="live-info-title">
+                          {info.title}
+                        </span>
+
+                        <div className="live-wininfo">
+                          <span className="live-win">
+                            WIN
+                          </span>
+                          <span className="pp-wrapper">
+                            <p className="pp-text">PP</p>
+                          </span>
+                          <span className="live-point">
+                            {info.point}
+                          </span>
+                        </div>
+                      </div>
+                      <div className="live-liveinfo">
+                        <div
+                          className="live-"
+                          style={{
+                            borderTop: "1px solid",
+                            padding: "20px 0",
+                            display: "flex",
+                            justifyContent: "space-between",
+                          }}
+                        >
+                          <span
+                            style={{
+                              border: "1px solid",
+                              padding: "7px",
+                              borderRadius: 10,
+                              backgroundColor: "red",
+                              color: "#fff",
+                              fontWeight: "bold",
+                            }}
+                          >
+                            Live
+                          </span>
+                          <span>People</span>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-            ))}
-          </Slider>
+                ))}
+              </Slider>
+            </>
+          )}
         </>
       )}
     </>
--- src/components/slides/MissionSlide.js
+++ src/components/slides/MissionSlide.js
@@ -2,6 +2,12 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile,
+} from "react-device-detect";
 
 const MissionSlide = ({ weekly, size }) => {
   const settings = {
@@ -21,107 +27,113 @@ const MissionSlide = ({ weekly, size }) => {
   };
 
   // useEffect(() => {
-
-  //   // const ban = Math.round(window.innerWidth * 0.0045);
-  //   // console.log("ban");
-  //   // console.log(ban);
-  //   // setFf(ban);
+  // const ban = Math.round(window.innerWidth * 0.0045);
+  // console.log("ban");
+  // console.log(ban);
+  // setFf(ban);
   // }, [ff]);
+
   return (
     <>
       {weekly !== undefined && (
         <>
-          <Slider {...settings}>
-            {weekly.map((info, index) => (
+          {<BrowserView /> ||
+            (<MobileView /> && (
               <>
-                <div
-                  style={{
-                    width: 220,
-                    position: "relative",
-                    margin: "0 0 15px 0",
-                  }}
-                  key={index}
-                >
-                  <img
-                    style={{
-                      position: "relative",
-                      width: "100%",
-                      height: 217,
-                      borderRadius: "16px",
-                      backgroundColor: "#9bc8d7",
-                      border: "none",
-                    }}
-                    src={info.img}
-                    alt=""
-                  />
-                  <div
-                    style={{
-                      fontFamily: "AppleSDGothicNeo",
-                      textAlign: "left",
-                      padding: "20px 0 0 0",
-                    }}
-                  >
-                    <div
-                      style={{
-                        fontSize: 32,
-                        fontWeight: "bold",
-                        color: "#09607b",
-                        display: "flex",
-                        alignItems: "center",
-                      }}
-                    >
-                      <span>To Win</span>
-                      <span
+                <Slider {...settings}>
+                  {weekly.map((info, index) => (
+                    <>
+                      <div
                         style={{
-                          width: 34,
-                          height: 34,
-                          border: "solid 1.5px #149231",
-                          backgroundColor: "#38ae53",
-                          margin: " 0 10px",
-                          color: "#fff",
-                          display: "flex",
-                          alignItems: "center",
-                          justifyContent: "center",
-                          borderRadius: 25,
+                          width: 220,
+                          position: "relative",
+                          margin: "0 0 15px 0",
                         }}
+                        key={index}
                       >
+                        <img
+                          style={{
+                            position: "relative",
+                            width: "100%",
+                            height: 217,
+                            borderRadius: "16px",
+                            backgroundColor: "#9bc8d7",
+                            border: "none",
+                          }}
+                          src={info.img}
+                          alt=""
+                        />
                         <div
                           style={{
-                            margin: "auto",
-                            fontSize: 22,
-                            fontWeight: 800,
-                            display: "flex",
-                            height: "88%",
-                            alignItems: "flex-end",
+                            fontFamily: "AppleSDGothicNeo",
+                            textAlign: "left",
+                            padding: "20px 0 0 0",
                           }}
                         >
-                          PP
+                          <div
+                            style={{
+                              fontSize: 32,
+                              fontWeight: "bold",
+                              color: "#09607b",
+                              display: "flex",
+                              alignItems: "center",
+                            }}
+                          >
+                            <span>To Win</span>
+                            <span
+                              style={{
+                                width: 34,
+                                height: 34,
+                                border:
+                                  "solid 1.5px #149231",
+                                backgroundColor: "#38ae53",
+                                margin: " 0 10px",
+                                color: "#fff",
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "center",
+                                borderRadius: 25,
+                              }}
+                            >
+                              <div
+                                style={{
+                                  margin: "auto",
+                                  fontSize: 22,
+                                  fontWeight: 800,
+                                  display: "flex",
+                                  height: "88%",
+                                  alignItems: "flex-end",
+                                }}
+                              >
+                                PP
+                              </div>
+                            </span>
+                            <span>{info.point}</span>
+                          </div>
+                          <div
+                            style={{
+                              fontSize: 30,
+                              fontWeight: 500,
+                              color: "#85b8c8",
+                            }}
+                          >
+                            Target {info.target}
+                          </div>
+                          <div
+                            style={{
+                              fontSize: 30,
+                              color: "#85b8c8",
+                            }}
+                          >
+                            START NOW
+                          </div>
                         </div>
-                      </span>
-                      <span>{info.point}</span>
-                    </div>
-                    <div
-                      style={{
-                        fontSize: 30,
-                        fontWeight: 500,
-                        color: "#85b8c8",
-                      }}
-                    >
-                      Target {info.target}
-                    </div>
-                    <div
-                      style={{
-                        fontSize: 30,
-                        color: "#85b8c8",
-                      }}
-                    >
-                      START NOW
-                    </div>
-                  </div>
-                </div>
+                      </div>
+                    </>
+                  ))}
+                </Slider>
               </>
             ))}
-          </Slider>
         </>
       )}
     </>
